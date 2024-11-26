06c9a4453d667f917c4a6ce86055c1cbd0b318d2
mohmmadsbeeh <mouhmmadsbieh@gmail.com>
2024-11-26 22:07:11 +0100

    launch file added

--- launch/cam_publisher_node_withMQTT_launch.py
+++ launch/cam_publisher_node_withMQTT_launch.py
@@ -11,7 +11,30 @@ def generate_launch_description():
         package='autoware_v2x_cam_publisher',
         executable='cam_publisher_node_withMQTT',
         name='cam_publisher_node_withMQTT',
-        parameters=['autoware_ws/src/autoware_v2x_cam_publisher/config/cam_publisher_with_mqtt_params.yaml'],
+        parameters=['autoware_ws/src/autoware_v2x_cam_publisher/config/cam_publisher_with_mqtt_params.yaml',
+                   {
+                'station_id': 1,  # Station ID
+
+                # Vehicle dimensions and parameters
+                'wheel_radius': 0.3,
+                'wheel_width': 0.2,
+                'wheel_base': 2.5,
+                'wheel_tread': 1.6,
+                'front_overhang': 0.8,
+                'rear_overhang': 0.8,
+                'left_overhang': 0.5,
+                'right_overhang': 0.5,
+                'vehicle_height': 1.5,
+                'vehicle_width': 1.8,
+                'vehicle_length': 4.1,  # wheel_base + front_overhang + rear_overhang
+                'max_steer_angle': 0.785,  # in radians (~45 degrees)
+
+                # Reference position for coordinate conversion
+                'reference_latitude': 0.0,
+                'reference_longitude': 0.0,
+                'reference_altitude': 0.0,
+            }
+],
         output='screen',
     )
 
